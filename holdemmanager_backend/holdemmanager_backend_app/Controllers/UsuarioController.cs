89f8b81a1e8f9229a3e6a21726b5892f99a2ead6
Rodrigo Denis <rodrigodenis631@gmail.com>
2024-06-01 20:44:24 -0300

    Primer commit

---
+++ holdemmanager_backend/holdemmanager_backend_app/Controllers/UsuarioController.cs
@@ -0,0 +1,101 @@
+﻿using BackEnd.Domain.IServices;
+using BackEnd.Domain.Models;
+using BackEnd.DTO;
+using BackEnd.Utils;
+using holdemmanager_backend_app.Persistence;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace BackEnd.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioService _usuarioService;
+        private readonly AplicationDbContext _dbContext;
+        public UsuarioController(AplicationDbContext dbContext, IUsuarioService usuarioService)
+        {
+
+            _usuarioService = usuarioService;
+            _dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Usuario usuario)
+        {
+
+            try
+            {
+                var validateExistence = await _usuarioService.ValidateExistence(usuario);
+                if (validateExistence)
+                {
+
+                    return BadRequest(new { message = "El usuario " + usuario.NombreUsuario + " ya existe" });
+
+                }
+
+                usuario.Password = Encriptar.EncriptarPassword(usuario.Password);
+                await _usuarioService.SaveUser(usuario);
+
+                return Ok(new { mesagge = "Usuario registrado con exito" });
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+
+            }
+
+        }
+
+        [Route("CambiarPassword")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpPut]
+        public async Task<IActionResult> CambiarPassword([FromBody] CambiarPasswordDTO cambiarPassword)
+        {
+            try
+            {
+                var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+                int idUsuario = JwtConfigurator.GetTokenIdUsuario(identity);
+                string passwordEncriptado = Encriptar.EncriptarPassword(cambiarPassword.passwordAnterior);
+                var usuario = await _usuarioService.ValidatePassword(idUsuario, passwordEncriptado);
+
+                if (usuario == null)
+                {
+
+                    return BadRequest(new { message = "Password incorrecta" });
+
+                }
+                else
+                {
+
+                    usuario.Password = Encriptar.EncriptarPassword(cambiarPassword.nuevaPassword);
+                    await _usuarioService.UpdatePassword(usuario);
+                    return Ok(new { message = "La password fue actualizada con exito" });
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Usuario>> GetUsers()
+        {
+            return await _dbContext.Usuarios.FirstOrDefaultAsync();
+        }
+
+    }
+}
---
+++ holdemmanager_backend/holdemmanager_backend_app/Utils/Encriptar.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BackEnd.Utils
+{
+    public static class Encriptar
+    {
+
+        public static string EncriptarPassword(string input)
+        {
+
+            MD5 md5Hash = MD5.Create();
+
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+
+                sBuilder.Append(data[i].ToString("x2"));
+
+            }
+            return sBuilder.ToString();
+        }
+    }
+}
