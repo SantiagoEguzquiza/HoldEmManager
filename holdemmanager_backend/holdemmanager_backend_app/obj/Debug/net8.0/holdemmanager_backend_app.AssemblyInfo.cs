f880b0df1cf16bce91c0ea10b502f0ae68bcd5a8
Rodrigo Denis <rodrigodenis631@gmail.com>
2024-06-02 16:55:10 -0300

    Debug config

--- holdemmanager_backend/holdemmanager_backend_app/obj/Debug/net8.0/holdemmanager_backend_app.AssemblyInfo.cs
+++ holdemmanager_backend/holdemmanager_backend_app/obj/Debug/net8.0/holdemmanager_backend_app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("holdemmanager_backend_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4583f3c02d26c864a0456ee9bacd35c8262c5c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d96786256468181de19920185d38e8935bea2d77")]
 [assembly: System.Reflection.AssemblyProductAttribute("holdemmanager_backend_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("holdemmanager_backend_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
