f6db60cb3547c05b646dfbffd0ed1583bbefea07
Rodrigo Denis <rodrigodenis631@gmail.com>
2024-06-24 20:20:14 -0300

    Cambios de carpetas

--- holdemmanager_web_frontend/src/app/components/recursos/recursos-edit-delete.component.spec.ts
+++ holdemmanager_web_frontend/src/app/components/recursos/recursos-edit-delete.component.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
-import { RecursosEditDeleteComponent } from './recursos-edit-delete.component';
+import { RecursosEditDeleteComponent } from '../../../recursos/recursos-edit-delete.component';
 
 describe('RecursosEditDeleteComponent', () => {
   let component: RecursosEditDeleteComponent;
