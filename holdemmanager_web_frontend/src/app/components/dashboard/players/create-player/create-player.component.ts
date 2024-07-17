b8c3ba2d46610bb05ddf780345215b2bf21566b5
SantiagoEguzquiza <santieguzquiza@gmail.com>
2024-07-16 23:20:31 -0300

    boton create player

--- holdemmanager_web_frontend/src/app/components/dashboard/players/create-player/create-player.component.ts
+++ holdemmanager_web_frontend/src/app/components/dashboard/players/create-player/create-player.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 interface Jugador {
   id: number;
@@ -15,23 +16,58 @@ interface Jugador {
   styleUrls: ['./create-player.component.css']
 })
 export class CreatePlayerComponent {
-  nuevoJugador: Jugador = {
-    id: 0,
-    numberPlayer: 0,
-    name: '',
-    email: '',
-    password: '',
-    imageUrl: ''
-  };
+  jugadorForm: FormGroup;
 
   @Output() guardar = new EventEmitter<Jugador>();
   @Output() cancelar = new EventEmitter<void>();
 
+  constructor(private fb: FormBuilder) {
+    this.jugadorForm = this.fb.group({
+      id: [0],
+      numberPlayer: ['', [Validators.required, Validators.pattern('^[0-9-]*$')]],
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email, this.gmailValidator]],
+      password: ['', Validators.required], 
+      imageUrl: ['']
+    });
+  }
+
+  gmailValidator(control: AbstractControl): {[key: string]: any} | null {
+    const email = control.value;
+    return email.endsWith('@gmail.com') ? null : { 'gmail': true };
+  }
+
   guardarJugador() {
-    this.guardar.emit(this.nuevoJugador);
+    if (this.jugadorForm.valid) {
+      this.guardar.emit(this.jugadorForm.value);
+    }
   }
 
   cancelarCreacion() {
     this.cancelar.emit();
+  }
+
+  get numberPlayer() {
+    return this.jugadorForm.get('numberPlayer');
+  }
+
+  get name() {
+    return this.jugadorForm.get('name');
+  }
+
+  get email() {
+    return this.jugadorForm.get('email');
+  }
+
+  get password() {
+    return this.jugadorForm.get('password');
+  }
+
+  get confirmPassword() {
+    return this.jugadorForm.get('confirmPassword');
+  }
+
+  get imageUrl() {
+    return this.jugadorForm.get('imageUrl');
   }
 }
