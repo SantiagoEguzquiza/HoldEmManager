411f1c00fb1c257e01d6623294462f324d4187c5
SantiagoEguzquiza <santieguzquiza@gmail.com>
2025-04-13 18:42:49 -0300

    Clients section ended

--- holdemmanager_backend/holdemmanager_backend_web/obj/Release/net8.0/holdemmanager_backend_web.AssemblyInfo.cs
+++ holdemmanager_backend/holdemmanager_backend_web/obj/Release/net8.0/holdemmanager_backend_web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("holdemmanager_backend_web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+091c09b889701f1886312941f813a248f60b2a60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7381baf42e5d2dfa9215d1c57cd17b6a02022b5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("holdemmanager_backend_web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("holdemmanager_backend_web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
